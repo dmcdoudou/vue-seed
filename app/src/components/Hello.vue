5525c0ddc79c801168ea9c506da54dcf7044ff98
JasonBai007 <byygkcg@163.com>
2017-05-03 10:28:37 +0800

    换视频

--- app/src/components/Hello.vue
+++ app/src/components/Hello.vue
@@ -5,7 +5,7 @@
       <h2 class="animated flipInX">front-end developer & photographer, welcome to my world.</h2>
     </div>
     <video autoplay loop id="vbg">
-      <source src="../../static/video/clouds2.webm" type="video/mp4">
+      <source src="../../static/video/subway.mp4" type="video/mp4">
     </video>
   </div>
 </template>
@@ -38,26 +38,25 @@ export default {
       }
     }
     h2 {
-      // font-size: 2em;
       margin: 10px;
     }
   }
   // 实现视频背景
   #vbg {
     position: fixed;
-    min-width: 90%;
-    min-height: 100%;
-    width: auto;
-    height: auto;
+    bottom: 0;
+    right: 0;
+    top: 62px;
+    left: 140px;
     z-index: -100;
     background: url(../assets/img/end.png) no-repeat;
     background-size: cover;
     // 以下是模糊滤镜效果
-    -webkit-filter: blur(10px);
-    -moz-filter: blur(10px);
-    -o-filter: blur(10px);
-    -ms-filter: blur(10px);
-    filter: blur(10px);
+    -webkit-filter: grayscale(100%);
+    -o-filter: grayscale(100%);
+    -moz-filter: grayscale(100%);
+    -ms-filter: grayscale(100%);
+    filter: grayscale(100%);
   }
 }
 </style>

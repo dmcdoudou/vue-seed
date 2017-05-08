f8c2f4a7b3df373c93face74a80bc56871c32d7c
JasonBai007 <byygkcg@163.com>
2017-05-08 16:58:59 +0800

    添加功能

--- app/src/components/common/Pie.vue
+++ app/src/components/common/Pie.vue
@@ -2,11 +2,14 @@
     <div class="pie-wrap">
         <h1>{{tl}}</h1>
         <el-row :gutter="20" type="flex" justify="center">
-            <el-col :span="12">
-                <el-input placeholder="项目"></el-input>
+            <el-col :span="10">
+                <el-input placeholder="项目" v-model="item.name"></el-input>
             </el-col>
-            <el-col :span="12">
-                <el-input-number @change="handleChange" :min="0" :max="10"></el-input-number>
+            <el-col :span="8">
+                <el-input-number :min="0" :max="100000000" v-model="item.value"></el-input-number>
+            </el-col>
+            <el-col :span="4">
+                <el-button @click="add">添加</el-button>
             </el-col>
         </el-row type="flex" justify="center">
         <div :id="rootid" style="width:560px;height:260px;" :nomeaning="pieData" ref="pieroot"></div>
@@ -26,13 +29,14 @@ export default {
     props: ['pieData', 'tl', 'rootid'],
     data() {
         return {
-            item: []
+            item: {
+                name:'',
+                value:0
+            },
+            items: []
         }
     },
     mounted() {
-        this.renderPie();
-    },
-    updated() {
         this.renderPie();
     },
     methods: {
@@ -46,22 +50,7 @@ export default {
                     name: '访问来源',
                     type: 'pie',
                     center: ['50%', '50%'],
-                    data: [{
-                        value: 335,
-                        name: '直接访问'
-                    }, {
-                        value: 310,
-                        name: '邮件营销'
-                    }, {
-                        value: 234,
-                        name: '联盟广告'
-                    }, {
-                        value: 135,
-                        name: '视频广告'
-                    }, {
-                        value: 1548,
-                        name: '搜索引擎'
-                    }],
+                    data: this.items,
                     itemStyle: {
                         emphasis: {
                             shadowBlur: 10,
@@ -74,12 +63,15 @@ export default {
             this.myChart = echarts.init(this.$refs.pieroot)
             this.myChart.setOption(opts)
         },
-        handleChange(v) {
-            console.log(v)
+        add(v) {
+            let one = JSON.parse(JSON.stringify(this.item))
+            this.items.push(one);
+            this.renderPie();
         }
     }
 }
 </script>
+
 <style lang="less" scoped>
 h1 {
     text-align: center;

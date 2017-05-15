005cf8e73a89d8722616e35fdffea3ec1572b900
JasonBai007 <byygkcg@163.com>
2017-05-15 16:43:09 +0800

    美化表单页

--- app/src/components/HandleSeed.vue
+++ app/src/components/HandleSeed.vue
@@ -9,20 +9,23 @@
                 <el-form-item label="序号">
                     <el-input v-model="form.id" :disabled="true"></el-input>
                 </el-form-item>
-                <el-form-item label="种子解析类型">
-                    <el-input v-model="form.parser"></el-input>
-                </el-form-item>
                 <el-form-item label="种子表达式">
                     <el-input v-model="form.key"></el-input>
+                </el-form-item>
+                <el-form-item label="种子状态">
+                    <el-switch v-model="form.status" on-text="ON" off-text="OFF"></el-switch>
+                </el-form-item>
+                <el-form-item label="种子解析类型">
+                    <el-select v-model="form.parser" placeholder="请选择活动区域">
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="爬虫类型">
                     <el-select v-model="form.type" placeholder="请选择爬虫类型">
                         <el-option label="区域一" value="shanghai"></el-option>
                         <el-option label="区域二" value="beijing"></el-option>
                     </el-select>
-                </el-form-item>
-                <el-form-item label="种子状态">
-                    <el-switch v-model="form.status" on-text="ON" off-text="OFF"></el-switch>
                 </el-form-item>
                 <el-form-item label="关联模板">
                     <el-select v-model="form.pageid" placeholder="请选择活动区域">
@@ -85,7 +88,10 @@ export default {
 </script>
 <style scoped lang="less">
 .form-wrap {
-    margin: 40px auto;
-    width: 60%
+    margin: 40px auto 0;
+    width: 50%
+}
+.el-select {
+    width: 100%
 }
 </style>

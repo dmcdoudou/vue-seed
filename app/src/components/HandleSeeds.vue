c14f3b84b2bb445ebfff76f6e690f2c5690ecc09
JasonBai007 <byygkcg@163.com>
2017-05-23 10:23:01 +0800

    完善种子管理表单页

--- app/src/components/HandleSeeds.vue
+++ app/src/components/HandleSeeds.vue
@@ -7,31 +7,28 @@
         <div class="form-wrap">
             <el-form ref="form" :model="form" label-width="120px">
                 <el-form-item label="序号">
-                    <el-input v-model="form.id" :disabled="true"></el-input>
+                    <el-input v-model="form.id" disabled placeholder="系统自动生成，不用填写"></el-input>
                 </el-form-item>
                 <el-form-item label="种子表达式">
-                    <el-input v-model="form.key"></el-input>
+                    <el-input v-model="form.key" placeholder="请填写种子表达式"></el-input>
                 </el-form-item>
                 <el-form-item label="种子状态">
                     <el-switch v-model="form.status" on-text="ON" off-text="OFF"></el-switch>
                 </el-form-item>
                 <el-form-item label="种子解析类型">
                     <el-select v-model="form.parser" placeholder="请选择种子解析类型">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
+                        <!-- 数据源是对象的时候，这么渲染出列表，要记得绑定和添加唯一的KEY -->
+                        <el-option v-for="(value, key, index) in parse_list" :label="key" :value="value" :key="index"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="爬虫类型">
-                    <el-select v-model="form.type" placeholder="请选择爬虫类型">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
+                    <el-select v-model="form.spider_type" placeholder="请选择爬虫类型">
+                        <el-option v-for="(value, key, index) in spider_list" :label="key" :value="value" :key="index"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="关联模板">
-                    <el-select v-model="form.pageid" placeholder="请选择关联模板">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
+                    <!-- 添加number修饰符将字符转成数字 -->
+                    <el-input v-model.number="form.pageid" placeholder="请填写关联模板ID"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="onSubmit">提交</el-button>
@@ -42,17 +39,11 @@
     </div>
 </template>
 <script>
-import Mock from 'mockjs'
-let debug = 0;
-if (debug) {
-    Mock.mock(/spidertypelist/, {
-
-    })
-}
-
 const ROOT_URL = 'http://172.20.207.28:5000';
 const GLOBAL_URL = {
-    submit: `${ROOT_URL}/api/spider/template/list`
+    parse_list: `${ROOT_URL}/api/spider/seed/parse`,
+    spider_list: `${ROOT_URL}/api/spider/type/list`,
+    submit: `${ROOT_URL}/api/spider/seed/update`
 }
 
 export default {
@@ -64,24 +55,52 @@ export default {
                 key: '',
                 parser: '',
                 pageid: '',
-                type: '',
+                spider_type: '',
                 status: true,
-            }
+            },
+            parse_list: {},
+            spider_list: {}
         }
     },
     created() {
-        let data = this.$route.query.data;
-        if (data) {
-            let pData = JSON.parse(data);
-            // 这是一个坑，文档里说开关的Model的类型只能是布尔型
-            pData.status = pData.status === 1 ? true : false
-            this.form = pData;
-        }
+        this.getParseList()
+        this.getSpiderList()
+        this.renderPage()
     },
     methods: {
+        getParseList() {
+            this.$http.get(GLOBAL_URL.parse_list).then(res => {
+                if (res.body.msg === 'Success') {
+                    this.parse_list = res.body.data
+                }
+            }, res => {
+                this.$message.error('网络错误，解析类型列表获取失败');
+            })
+        },
+        getSpiderList() {
+            this.$http.get(GLOBAL_URL.spider_list).then(res => {
+                if (res.body.msg === 'Success') {
+                    this.spider_list = res.body.data
+                }
+            }, res => {
+                this.$message.error('网络错误，爬虫类型列表获取失败');
+            })
+        },
+        renderPage() {
+            let data = this.$route.query.data;
+            // 如果URL中有data数据，则是编辑页面
+            if (data) {
+                let pData = JSON.parse(data);
+                // 这是一个坑，文档里说开关的Model的类型只能是布尔型
+                pData.status = pData.status === 1 ? true : false
+                this.form = pData;
+            }
+        },
         onSubmit() {
+            let postData = JSON.parse(JSON.stringify(this.form))
+            postData.status = postData.status === true ? 1 : 0
             this.$http.post(GLOBAL_URL.submit, {
-                params: this.form
+                params: postData
             }).then(res => {
                 if (res.body.msg === 'Success') {
                     this.$message.success('编辑成功！');
@@ -102,6 +121,7 @@ export default {
     margin: 40px auto 0;
     width: 50%
 }
+
 .el-select {
     width: 100%
 }
